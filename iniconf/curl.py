3d5c4fc512b7830a86631a163761d23d73977081
Docun <167471277+Docun@users.noreply.github.com>
2024-07-28 13:54:01 +0300

    test_conf

---
+++ iniconf/curl.py
@@ -0,0 +1,4 @@
+login_page = 'https://stellarburgers.nomoreparties.site/login'
+main_page = 'https://stellarburgers.nomoreparties.site/'
+profile_page = 'https://stellarburgers.nomoreparties.site/account/profile'
+register_page = 'https://stellarburgers.nomoreparties.site/register'
